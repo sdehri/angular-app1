60652eae99b96885531653733ea3cd9326e4c804
sdehri <71267763+sdehri@users.noreply.github.com>
2020-12-03 09:21:16 +0530

    Update factory.js

--- User List/factory.js
+++ User List/factory.js
@@ -1,17 +1,3 @@
 app.factory('user', ['$http', function($http) {
-    var all = [];
-	return{
-    getData : function() {
-        return $http.get("https://jsonplaceholder.typicode.com/users");
-		
-	}
-	}
-      /*   .then(function(response) {
-          all = response.data;
-          return all;
-        }); */
-  /*  }
-     return {
-        getData: getData 
-    }; */
+   /* Add code here */
 }]);
