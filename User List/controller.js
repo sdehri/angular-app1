fe8f60a7493399b1405b6a121f8347846b6f7574
sdehri <71267763+sdehri@users.noreply.github.com>
2020-12-03 09:20:48 +0530

    Update controller.js

--- User List/controller.js
+++ User List/controller.js
@@ -1,39 +1,5 @@
 var app = angular.module('myapp', []);
-app.controller('mycontroller', function($scope, $http, user) {
-    $scope.btname = 'ADD';
-    $scope.sbtname = 'GET ALL';
-	$scope.collection = []; //[{name: "Bob", phone:"989898989111"}, {name:"Tom", phone:"98227272222"}];
-
-    $scope.insertIntoList = function() {
-
-
-            if ($scope.name == null) {
-                alert('insert Name')
-            } else if ($scope.phone == null) {
-                alert('insert Phone')
-            } else {
-				$scope.collection.push({name: $scope.name, phone: $scope.phone});			
-            }
-
-        }
-		
-	$scope.getList= function()  {
-
-	$scope.collection = [];
-	user.getData().then(
-    function resolved (response) {
-        var all = response.data;
-		angular.forEach(all, function(c, i) {
-		$scope.collection.push({name: c.name, phone: c.phone});
-	})
-    },
-    function rejected (response) {
-        alert(response.status + ': ' + response.statusText);
-    }
-);
-	
-	
-	}	
-
+app.controller('mycontroller', function($scope) {
+ /* Add code here */
 
 })
